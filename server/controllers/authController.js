1e8e8f230cfe98ca9ce84ad4a1af9699639b74a8
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-16 06:38:17 -0300

    login api added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -27,9 +27,38 @@ export const register = async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' })
 
         res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite:process.env.NODE_ENV === 'production' ? 'None' : 'strict', maxAge: 7 * 24 * 60 * 60 * 1000 })
-       
+        return res.json({ success: true, message: 'User registered successfully', user })
 
     } catch (error) {
         res.json({ success: false, message: error.message})
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        return res.json({ success: false, message: 'All fields are required' })
+    }
+
+    try {
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.json({ success: false, message: 'Invalid email' })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.json({ success: false, message: 'Invalid  password' })
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' })
+
+        res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite:process.env.NODE_ENV === 'production' ? 'None' : 'strict', maxAge: 7 * 24 * 60 * 60 * 1000 })
+
+        res.json({ success: true, message: 'User logged in successfully', user })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
     }
 }
