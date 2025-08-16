4e4cf23cbe15cbf602335b92a77d7b4be4539c7f
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-16 06:48:48 -0300

    register,login and logout api routed properly

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import cookieParser from 'cookie-parser'
 import connectDB from './configs/mongodb.js'
+import authRouter from './routes/authRoutes.js'
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -12,9 +13,12 @@ app.use(cors({credentials:true}))
 app.use(express.json())
 app.use(cookieParser())
 
+//api endpoints
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/api/auth', authRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
