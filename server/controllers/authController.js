47c26cc5ed52958d5ba9be4a7a7ab9588348a0b3
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-16 06:41:30 -0300

    logout api added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -62,3 +62,12 @@ export const login = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie('token', { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite:process.env.NODE_ENV === 'production' ? 'None' : 'strict' })
+        return res.json({ success: true, message: 'User logged out successfully' })
+    } catch (error) {
+        return res.json({ success: false, message: error.message })
+    }
+}
